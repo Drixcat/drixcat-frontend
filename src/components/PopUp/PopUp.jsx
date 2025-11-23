5d99622c1cb55559080e6c01d78f675bce1fdd5b
Mohd. Shadav <muhammadshadav166@gmail.com>
2025-11-23 22:04:52 +0530

    commit 23/11/25 - text increase popup

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -239,7 +239,7 @@ function PopUp({onClose,onSubmitSuccess}) {
           </div> : <div className="">
             <p
               
-              className="text-[#099f4e] text-center px-6 py-2 hover:opacity-90 transition"
+              className="text-xl text-[#099f4e] text-center px-6 py-2 hover:opacity-90 transition"
             >
               {`Email sent! We’ll get back to you soon.`}
             </p>
